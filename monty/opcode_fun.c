c469aab165fe106fc2f43539ff686b8060a3494b
Jikrou <jamaleddine.zait@gmail.com>
2024-03-29 03:21:11 +0000

    bytecode inter

---
+++ monty/opcode_fun.c
@@ -0,0 +1,110 @@
+#include "monty.h"
+
+instruction_t instructions[] = {
+    {"pall", *pall}, {"pint", *pint}, {"pop", pop},
+    {"swap", *swap}, {"add", *add},   {"nop", *nop},
+
+};
+/**
+ * check_instructions - Checks if the given opcode matches any
+ * supported instruction and executes the corresponding function.
+ * @stack: Pointer to the beginning of the stack.
+ * @opcode: String representing the opcode of the instruction to be executed.
+ * @arg: Argument associated with the instruction (if any).
+ * @line_number: Line number being executed.
+ */
+void check_instructions(stack_t *stack, char *opcode, char *arg,
+                        unsigned int line_number) {
+  size_t i;
+  if (stack != NULL || opcode != NULL) {
+    for (i = 0; i < sizeof(instructions) / sizeof(instructions[0]); i++) {
+      if (strcmp(opcode, instructions[i].opcode) == 0) {
+        instructions[i].f(&stack, line_number);
+        return;
+      }
+    }
+  }
+
+  fprintf(stderr, "Error at line %u: Unknown instruction %s\n", line_number,
+          opcode);
+}
+/**
+ * push - The opcode push pushes an element to the stack.
+ * @stack: double pointer to the top of the stack
+ * @line_number: the line number of the opcode
+ */
+void push(stack_t **stack, int data) {
+  stack_t *newNode = malloc(sizeof(stack_tt));
+  if (newNode == NULL) {
+    fprintf(stderr, "Memory allocation failed\n");
+    exit(EXIT_FAILURE);
+  }
+  newNode->n = data;
+  newNode->prev = NULL;
+  newNode->next = *stack;
+
+  if (*stack != NULL) {
+    (*stack)->prev = newNode;
+  }
+
+  *stack = newNode;
+}
+/**
+ * pall - The opcode pall prints all the values on the stack,
+ * starting from the top of the stack.
+ * @stack: double pointer to the top of the stack
+ * @line_number: the line number of the opcode
+ */
+void pall(stack_t **stack, unsigned int line_number) {
+  stack_t *cur;
+
+  if (*stack == NULL) {
+    printf("Stack is empty\n");
+    return;
+  }
+
+  printf("Stack elements:\n");
+  cur = *stack;
+  while (cur != NULL) {
+    printf("%d\n", cur->n);
+    cur = cur->next;
+  }
+}
+
+/**
+ * pint - The opcode pint prints the value at the top of the stack,
+ * followed by a new line.
+ * @stack: double pointer to the top of stack
+ * @line_number: the line number of the opcode
+ */
+void pint(stack_t **stack, unsigned int line_number) {
+  stack_t *cur;
+
+  if (*stack == NULL) {
+    fprintf(stderr, "L%u : can't pint, stack empty\n", line_number);
+    exit(EXIT_FAILURE);
+  }
+  cur = *stack;
+  if (cur != NULL)
+    printf("%d\n", cur->n);
+}
+
+/**
+ * pop - The opcode swap swaps the top two elements of the stack.
+ * @stack: double pointer to the top of the stack
+ * @line_number: the line number of the op
+ */
+void pop(stack_t **stack, unsigned int line_number) {
+  stack_t *cur = *stack;
+
+  if (cur == NULL) {
+    fprintf(stderr, "L%u : can't pop an empty stack\n", line_number);
+    exit(EXIT_FAILURE);
+  }
+  *stack = cur->prev;
+
+  if (*stack != NULL) {
+    (*stack)->next = NULL;
+  }
+  free(cur);
+}
---
+++ monty/opcode_fun1.c
@@ -0,0 +1,61 @@
+#include "monty.h"
+/**
+ * swap - Swaps the top two elements of the stack.
+ * @stack: Double pointer to the beginning of the stack.
+ * @line_number: Line number being executed.
+ */
+void swap(stack_t **stack, unsigned int line_number) {
+  stack_t *top = *stack;
+  int temp;
+
+  if (top == NULL || top->next == NULL) {
+    fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+    exit(EXIT_FAILURE);
+  }
+
+  temp = top->n;
+  top->n = top->next->n;
+  top->next->n = temp;
+}
+
+/**
+ * add - The opcode add adds the top two elements of the stack
+ * @stack: double pointer to the beginning of the stack
+ * @line_number: Line number being executed.
+ * Return: void
+ */
+void add(stack_t **stack, unsigned int line_number) {
+  stack_t *top = *stack;
+  int sum;
+  if (top == NULL || top->next == NULL) {
+    fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+    exit(EXIT_FAILURE);
+  }
+  sum = top->n + top->next->n;
+
+  top = *stack;
+  top->n = sum;
+}
+/**
+ * nop - The opcode nop doesn’t do anything.
+ * @stack: double pointer to the beginning of the stack.
+ * @line_number: Line number being executed.
+ */
+void nop(stack_t **stack, unsigned int line_number) {
+  (void)stack;
+  (void)line_number;
+}
+
+/**
+ * free_stack - a function that frees all the stack
+ * @head: pointer to the head of the stack
+ */
+void free_stack(stack_t *head) {
+  stack_t *curr;
+
+  while (head != NULL) {
+    curr = head->next;
+    free(head);
+    head = curr;
+  }
+}
