3836fab5fba0cca5afb40e2c7535b458f7385c31
Jikrou <jamaleddine.zait@gmail.com>
2024-03-31 22:44:02 +0000

    mul

--- opcode_fun2.c
+++ opcode_fun2.c
@@ -19,9 +19,31 @@ void divi(stack_t **stack, unsigned int line_number)
 		print_err("division by zero", line_number);
 	}
 
-	num = top->n / top->next->n;
+	num = top->next->n / top->n;
 	pop(stack, line_number);
 
 	top = *stack;
 	top->n = num;
+}
+/**
+ * mult - multiplies the second top element of the stack with the top
+ * element of the stack.
+ *@stack: double pointer to the beginning of the stack
+ *@line_number: line number being evaluated from monty file
+*/
+void mult(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top = *stack;
+	int sum;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	sum = top->next->n * top->n;
+	pop(stack, line_number);
+
+	top = *stack;
+	top->n = sum;
 }
